c8d4ef0a398b379caddfcef3d3f2985cfa1ba9b5
Sergiognlz-DAM <Sergiognlz@gmail.com>
2026-06-16 13:39:55 +0200

    Update fix-paths.js

--- cineclip/fix-paths.js
+++ cineclip/fix-paths.js
@@ -5,9 +5,10 @@ const indexPath = path.join(__dirname, 'dist', 'index.html');
 let html = fs.readFileSync(indexPath, 'utf8');
 
 // Reemplaza /_expo/ por /TFG-SergioGonzalez/_expo/
-html = html.replace(/\/_expo\//g, '/TFG-SergioGonzalez/_expo/');
+html = html.replace(/\/_expo\//g, '/cineclip-/_expo/');
+html = html.replace(/href="\/favicon/g, 'href="/cineclip-/favicon');
 // Reemplaza /favicon por /TFG-SergioGonzalez/favicon
-html = html.replace(/href="\/favicon/g, 'href="/TFG-SergioGonzalez/favicon');
+html = html.replace(/href="\/favicon/g, 'href="/cineclip-/favicon');
 
 fs.writeFileSync(indexPath, html);
 console.log('✅ Paths corregidos en index.html');
